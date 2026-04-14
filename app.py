e0bbb8d40d3220456e39f5f88196ef3ea0997b95
Krutika <patilkru45@gmail.com>
2026-04-14 15:51:05 +0530

    Welcome Message

--- app.py
+++ app.py
@@ -9,3 +9,17 @@ print("Sum:", add(5, 3))
 
 def greet(name):
     return f"Hello, {name}!!!"
+
+def welcome(name):
+    return f"Welcome, {name}"
+
+def main():
+    print("Welcome App")
+
+    name = input("Enter your name: ")
+    message = welcome(name)
+
+    print(message)
+
+if __name__ == "__main__":
+    main()
