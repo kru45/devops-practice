a52c14db94e6c9ec21c2a52f70a4b20e519072cc
Krutika <patilkru45@gmail.com>
2026-04-14 16:18:23 +0530

    Updated message

--- app.py
+++ app.py
@@ -4,15 +4,4 @@ def greet(name):
 def add(a, b):
     return a + b
 
-print(greet("DevOps Beginner"))
-print("Sum:", add(5, 3))
-
-def greet(name):
-    return f"Hello, {name}"
-
-def main():
-    name = input("Enter your name: ")
-    print(greet(name))
-
-if __name__ == "__main__":
-    main()
+print("Git Remote Practice")
